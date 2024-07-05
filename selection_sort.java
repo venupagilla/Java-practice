87a3b377533ef9264bd84df7386270dc8004cf0d
venupagilla <pagillavenu909@gmail.com>
2024-07-05 10:43:10 +0530

    created new selection sort programm

---
+++ selection_sort.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class selection_sort{
+    public static void main(String args[]){
+        //declaring and defining an array
+        int[] array={88,97,44,12,5};
+
+        //printing the array without sorting
+        System.out.println(Arrays.toString(array));
+
+
+    }
+}
