205f7530358fc6d9d538dce3a878b75d58e14e69
venupagilla <pagillavenu909@gmail.com>
2024-07-14 22:54:45 +0530

    new program for searching using binary search in 2d arrays

---
+++ BinarySearchIn2dArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class BinarySearchIn2dArray {
+    public static void main(String args[]){
+        int[][] array1={
+            {10,20,30,40},
+            {50,60,70,80},
+            {90,100,110,120},
+            {130,140,150,160}
+        };
+        System.out.println(Arrays.toString(binarysearching(array1,120)));
+    }
+
+    static int[] binarysearching(int[][] array2,int target){
+        int row=0;
+        int column=array2[0].length-1;
+        while(row<array2.length && column>=0){
+            if(array2[row][column]==target){
+                return new int[]{row,column};
+            }
+            else if(target > array2[row][column]){
+                row++;;
+            }
+            else{
+                column--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
